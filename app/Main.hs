f1113a9e2a68d8b8befbf126c756cebd7d8a4135
Maciej Bielecki <zyla@prati.pl>
2024-04-09 16:23:14 +0000

    Switch replication direction

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,14 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
 import System.Environment (getArgs)
 import Network.Socket (getAddrInfo, defaultHints, AddrInfo (..), SocketType (..))
 import qualified Network.Socket  as Socket
-import Control.Monad (forM_, when, void)
+import Control.Monad (forM_, when, void, forever, forM)
 import Control.Exception
 import GHC.IO.IOMode (IOMode(ReadWriteMode))
 import GHC.IO.Handle (hSetBuffering, BufferMode (..), hFlush)
@@ -22,10 +23,20 @@ import Data.List (intercalate, sortOn)
 import Data.Text (Text)
 import qualified Network.URI.Encode as URIEncode
 import System.Random (randomIO)
-import Data.Maybe (isJust, listToMaybe, mapMaybe)
-import Control.Concurrent.Async (forConcurrently_)
+import Data.Maybe (isJust, catMaybes)
+import Control.Concurrent.Async (forConcurrently, concurrently_, mapConcurrently_)
 import Data.Bits
 import Data.Word
+import qualified Database.Memcache.Cluster as MC
+import Control.Concurrent (newMVar, threadDelay, forkIO)
+import Control.Concurrent.MVar (swapMVar)
+import qualified Data.Vector as V
+import qualified Database.Memcache.Server as MC
+import Data.IORef (newIORef, readIORef, writeIORef)
+import qualified Data.Map as Map
+import Text.Read (readMaybe)
+import Database.Memcache.Types (Expiration)
+import qualified Database.Memcache.Types as MC
 
 pattern FLAG_REPLICATED :: Word32
 pattern FLAG_REPLICATED = 1
@@ -36,62 +47,122 @@ main :: IO ()
 main = do
   [hostname, self] <- getArgs
   let hints = defaultHints { addrSocketType = Stream }
-  addrs <- sortOn addrAddress <$> getAddrInfo (Just hints) (Just hostname) (Just "11211")
 
   selfClient <- MC.newClient [MC.def { MC.ssHost = self }] MC.def
 
-  forConcurrently_ addrs \addr -> do
-    bracket (Socket.openSocket addr) Socket.close \s1 -> do
-      let ip = showIP (addrAddress addr)
-      client <- MC.newClient [MC.def { MC.ssHost = ip }] MC.def
+  let getServers = do
+        addrs <- sortOn addrAddress <$> getAddrInfo (Just hints) (Just hostname) (Just "11211")
+        catMaybes <$> forConcurrently addrs \addr -> do
+          isSelf <- bracket (MC.newClient [toServerSpec addr] MC.def) MC.quit (isSameAs selfClient)
+          if isSelf then do
+            pure Nothing
+          else
+            pure $ Just addr
 
-      r <- Text.encodeUtf8 . Text.pack . show <$> randomIO @Int
-      _ <- MC.set selfClient r "" FLAG_REPLICATED 3600
-      result <- MC.get client r
+      toServerSpec addr = MC.def { MC.ssHost = showIP (addrAddress addr) }
 
-      if isJust result then
-        putStrLn $ ip <> " is self, skipping"
-      else do
-        putStrLn $ ip <> " is not self, copying"
-        Socket.connect s1 (addrAddress addr)
-        h <- Socket.socketToHandle s1 ReadWriteMode
-        hSetBuffering h (BlockBuffering Nothing)
+  addrs <- getServers
 
-        let copy mode = do
-              fix \loop -> do
-                line <- Text.strip <$> Text.hGetLine h
-                forM_ (parseLine line) \key -> do
-                  let key' = Text.encodeUtf8 key
-                  m_value <- MC.get client key'
-                  forM_ m_value \(value, flags, _) -> do
-                    if mode == Watch && flags .&. FLAG_REPLICATED /= 0 then
-                      Text.putStrLn $ "got replicated change of " <> key <> ", ignoring"
-                    else do
-                      Text.putStrLn line
-                      when (mode == Watch) do
-                        Text.putStrLn $ "replicating change of " <> key <> " from " <> Text.pack ip
-                      void $ MC.set selfClient key' value (flags .|. FLAG_REPLICATED) 0
+  serversCache <- newIORef mempty
+  serversMVar <- newMVar mempty
 
-                when (line /= "" && line /= "END") loop
+  let updateServers = do
+        oldServers <- readIORef serversCache
+        addrs' <- getServers
+        servers <- forM addrs' \addr -> do
+          let key = showIP (addrAddress addr)
+          server <- case Map.lookup key oldServers of
+            Just s -> pure s
+            Nothing -> do
+              putStrLn $ "Discovered new server " <> key
+              MC.newServerDefault $ toServerSpec addr
+          pure (key, server)
+        writeIORef serversCache (Map.fromList servers)
+        void $ swapMVar serversMVar (V.fromList $ fmap snd servers)
 
-        Text.hPutStrLn h "lru_crawler metadump all"
-        hFlush h
-        copy Dump
+  updateServers
+  void $ forkIO $ forever do
+    threadDelay 5000000
+    updateServers
 
-        putStrLn $ "watching " <> ip
-        Text.hPutStrLn h "watch mutations"
-        hFlush h
-        line <- Text.strip <$> Text.hGetLine h
-        if line == "OK" then
-          copy Watch
-        else
-          putStrLn $ "'watch mutations' failed: " <> show line
+  allClient <- flip MC.setServers (Left serversMVar) <$> MC.newClient [] MC.def
+
+  let watchSelf = do
+        [addr] <- getAddrInfo (Just hints) (Just self) (Just "11211")
+        bracket (Socket.openSocket addr) Socket.close \s1 -> do
+          Socket.connect s1 (addrAddress addr)
+          h <- Socket.socketToHandle s1 ReadWriteMode
+          hSetBuffering h (BlockBuffering Nothing)
+
+          Text.hPutStrLn h "watch mutations"
+          hFlush h
+          do
+            line <- Text.strip <$> Text.hGetLine h
+            when (line /= "OK") do
+              putStrLn $ "'watch mutations' failed: " <> show line
+
+          fix \loop -> do
+            line <- Text.strip <$> Text.hGetLine h
+            forM_ (parseWatchMutationsLine line) \(key, ttl) -> do
+              let key' = Text.encodeUtf8 key
+              m_value <- MC.get selfClient key'
+              forM_ m_value \(value, flags, _) -> do
+                if flags .&. FLAG_REPLICATED /= 0 then
+                  Text.putStrLn $ "got replicated change of " <> key <> ", ignoring"
+                else do
+                  Text.putStrLn $ "replicating change of " <> key
+                  handle (\(e :: SomeException) -> putStrLn (show e)) $
+                    void $ MC.allOp allClient $ MC.emptyReq { MC.reqOp = MC.ReqSet MC.Loud key' value (MC.SESet (flags .|. FLAG_REPLICATED) ttl) }
+
+            when (line /= "" && line /= "END") loop
+
+      copyFromServer addr = do
+        let ip = showIP (addrAddress addr)
+        client <- MC.newClient [toServerSpec addr] MC.def
+
+        putStrLn $ "copying from " <> ip
+
+        bracket (Socket.openSocket addr) Socket.close \s1 -> do
+          Socket.connect s1 (addrAddress addr)
+          h <- Socket.socketToHandle s1 ReadWriteMode
+          hSetBuffering h (BlockBuffering Nothing)
+
+          Text.hPutStrLn h "lru_crawler metadump all"
+          hFlush h
+
+          fix \loop -> do
+            line <- Text.strip <$> Text.hGetLine h
+            forM_ (parseMetadumpLine line) \(key, expirationTime) -> do
+              let key' = Text.encodeUtf8 key
+              m_value <- MC.get client key'
+              forM_ m_value \(value, flags, _) -> do
+                void $ MC.set selfClient key' value (flags .|. FLAG_REPLICATED) expirationTime
+
+            when (line /= "" && line /= "END") loop
+
+  concurrently_ watchSelf (mapConcurrently_ copyFromServer addrs)
       
-parseLine :: Text -> Maybe Text
-parseLine t =
-  fmap URIEncode.decodeText $
-  listToMaybe $
-  mapMaybe (Text.stripPrefix "key=") $ Text.split (== ' ') t
+isSameAs :: MC.Client -> MC.Client -> IO Bool
+isSameAs selfClient client = do
+      randomKey <- Text.encodeUtf8 . Text.pack . show <$> randomIO @Int
+      _ <- MC.set selfClient randomKey "" FLAG_REPLICATED 3600
+      result <- MC.get client randomKey
+      _ <- MC.delete selfClient randomKey 0
+      pure $ isJust result
+
+parseMetadumpLine :: Text -> Maybe (Text, Expiration)
+parseMetadumpLine t = do
+  a:b:_ <- pure $ Text.split (== ' ') t
+  key <- Text.stripPrefix "key=" a
+  expirationTime <- Text.stripPrefix "exp=" b >>= readMaybe . Text.unpack
+  pure (URIEncode.decodeText key, expirationTime)
+
+parseWatchMutationsLine :: Text -> Maybe (Text, Expiration)
+parseWatchMutationsLine t = do
+  _ts:_gid:_type:a:_status:_cmd:b:_ <- pure $ Text.split (== ' ') t
+  key <- Text.stripPrefix "key=" a
+  expirationTime <- Text.stripPrefix "ttl=" b >>= readMaybe . Text.unpack
+  pure (URIEncode.decodeText key, expirationTime)
 
 showIP :: Socket.SockAddr -> String
 showIP = \case
